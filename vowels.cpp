37a40b548e1965189b9b2749b72382fbc3f5f3b4
harini S <hariniiyer97@gmail.com>
2018-09-02 00:17:29 +0530

    second commit

---
+++ vowels.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char a;
+   cin>>a;
+    if(a=='a'||a=='A'||a=='e'||a=='E'||a=='i'||a=='I'||a=='o'||a=='O'||a=='u'||a=='U')
+  
+    cout<<"vowels";
+    else
+    cout<<"consonants";
+   
+    
+	return 0;
+}
